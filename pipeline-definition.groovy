30a8bbe57d83788975aff3c234d512af3db1ac82
Andrew Zakharchenko <andrew.zakharchenko@merck.com>
2020-12-15 19:07:50 +0100

    Add jenkins configuration

---
+++ pipeline-definition.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+    tools {
+        maven 'maven'
+        jdk 'JDK11'
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'solution', url: 'https://github.com/ABlack-git/flightlog-spring-app'
+            }
+        }
+        stage('Build') {
+            steps {
+                dir('.') {
+                    sh "mvn --batch-mode clean install -DskipTests"
+                }
+            }
+        }
+        stage('Test') {
+            steps {
+                dir('.') {
+                    script {
+                        try {
+                            sh "mvn --batch-mode test -Dmaven.test.failure.ignore=true"
+                        } finally {
+                            junit '**/target/surefire-reports/*.xml'
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
